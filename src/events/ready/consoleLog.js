f7b3b9853776d0413914f73ac1cf90c2890b8fc5
Anthony <tghuy2000@yahoo.com>
2025-11-28 22:28:25 -0600

    Add dynamic bot status with rotating Sonic quotes.

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,52 +1,41 @@
-// const sonicQuotes = [
-//     'I have a small business to do!',
-//     'Gotta Go Fastt',
-//     'I\'m a hedgehog. I thought that was obvious.',
-//     'Nobody\'s gonna take my friends from me. Except me.',
-//     'Uh, meow?',
-//     'I\'ve been living my best life on Earth!',
-//     'This is my power… and I\'m not using it to run away anymore.'
-//   ];
+const sonicQuotes = [
+    '⚡️I have a small business to do!',
+    '⚡️Gotta Go Fastt',
+    'I\'m a hedgehog. I thought that was obvious.',
+    '⚡️Nobody\'s gonna take my friends from me. Except me.',
+    'Uh, meow?',
+    '⚡️I\'ve been living my best life on Earth!',
+    '⚡️This is my power… and I\'m not using it to run away anymore.'
+  ];
   
   module.exports = (client) => {
     console.log(`${client.user.tag} is online`);
   
     // Function to select a random quote from the sonicQuotes array
-    // function getRandomQuote() {
-    //   const randomIndex = Math.floor(Math.random() * sonicQuotes.length);
-    //   return sonicQuotes[randomIndex];
-    // }
+    function getRandomQuote() {
+      const randomIndex = Math.floor(Math.random() * sonicQuotes.length);
+      return sonicQuotes[randomIndex];
+    }
   
-    // Set Rich Presence
-    function setRichPresence() {
-      // const randomQuote = getRandomQuote();
+    // Set bot presence (bots cannot use Rich Presence features)
+    function setBotPresence() {
+      const randomQuote = getRandomQuote();
       
       client.user.setPresence({
         activities: [{
-          name: 'Sonic the Hedgehog',
-          type: 0, // Playing
-          details: 'Sonic The Hedgehog',
-          state: 'Playing Solo',
-          timestamps: {
-            start: Date.now() // Shows "elapsed" time since bot started
-          },
-          assets: {
-            large_image: 'sonic_knuckle_tail', // Large image: Sonic, Knuckles, and Tails
-            large_text: 'Numbani',
-            small_image: 'christmas_sonic', // Small image: Christmas Sonic
-            small_text: 'Rogue - Level 100',
-          }
+          name: randomQuote, // This is what will show as "Playing [quote]"
+          type: 0, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 5 = Competing
         }],
-        status: 'online'
+        status: 'online' // online, idle, dnd, invisible
       });
     }
   
-    // Set the initial Rich Presence when the bot is ready
-    setRichPresence();
+    // Set the initial presence when the bot is ready
+    setBotPresence();
   
-    // Update Rich Presence with a new random quote every 20 seconds
-    // setInterval(() => {
-    //   setRichPresence();
-    // }, 20000);
+    // Update presence with a new random quote every 20 seconds
+    setInterval(() => {
+      setBotPresence();
+    }, 20000);
   };
   
