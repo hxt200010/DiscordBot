4e04c82d89b11b1656bc8774f9fe668db62cfb33
Anthony <tghuy2000@yahoo.com>
2025-11-28 21:40:18 -0600

    feat: Add ready event handler to log bot online status and set rotating Sonic rich presence.

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,33 +1,49 @@
-const activities = [
-    '⚡ Gotta Go Fastt⚡',
-    '⚡ I\'m a hedgehog. I thought that was obvious.⚡',
-    '⚡ Nobody\'s gonna take my friends from me. Except me.⚡',
-    '⚡ Uh, meow?⚡',
-    '⚡ I\'ve been living my best life on Earth!⚡',
-    '⚡ This is my power… and I\'m not using it to run away anymore.⚡'
-  ];
+// const sonicQuotes = [
+//     'I have a small business to do!',
+//     'Gotta Go Fastt',
+//     'I\'m a hedgehog. I thought that was obvious.',
+//     'Nobody\'s gonna take my friends from me. Except me.',
+//     'Uh, meow?',
+//     'I\'ve been living my best life on Earth!',
+//     'This is my power… and I\'m not using it to run away anymore.'
+//   ];
   
   module.exports = (client) => {
     console.log(`${client.user.tag} is online`);
   
-    // Function to select a random activity from the activities array
-    function getRandomActivity() {
-      const randomIndex = Math.floor(Math.random() * activities.length);
-      return activities[randomIndex];
+    // Function to select a random quote from the sonicQuotes array
+    // function getRandomQuote() {
+    //   const randomIndex = Math.floor(Math.random() * sonicQuotes.length);
+    //   return sonicQuotes[randomIndex];
+    // }
+  
+    // Set Rich Presence
+    function setRichPresence() {
+      // const randomQuote = getRandomQuote();
+      
+      client.user.setPresence({
+        activities: [{
+          name: 'Sonic the Hedgehog',
+          type: 0, // Playing
+          details: 'Sonic the Hedgehog is here!!',
+          state: 'I have a small business to do!', // Static state
+          timestamps: {
+            start: Date.now() // Shows "elapsed" time since bot started
+          },
+          assets: {
+            large_image: 'f07219f0-255f-49ff-a0b6-20b51a7ae3e2', // Your Rich Presence asset key
+            large_text: 'Sonic the Hedgehog',
+          }
+        }],
+        status: 'online'
+      });
     }
   
-    // Set a random activity for the bot's presence
-    function setRandomPresence() {
-      const randomActivity = getRandomActivity();
-      client.user.setPresence({ activities: [{ name: randomActivity }], status: 'online' });
-    }
+    // Set the initial Rich Presence when the bot is ready
+    setRichPresence();
   
-    // Set the initial presence when the bot is ready
-    setRandomPresence();
-  
-    // Set a new random activity every 10 seconds (10000 milliseconds)
-    setInterval(() => {
-      setRandomPresence();
-    }, 20000);
+    // Update Rich Presence with a new random quote every 20 seconds
+    // setInterval(() => {
+    //   setRichPresence();
+    // }, 20000);
   };
-  
