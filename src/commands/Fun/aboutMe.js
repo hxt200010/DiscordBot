bc81053e96b2f0a26edef76ecd7126b9541d2b3b
Anthony <tghuy2000@yahoo.com>
2025-11-29 00:08:22 -0600

    add `aboutme` command to introduce Sonic the Hedgehog with an embed and video.

--- src/commands/Fun/aboutMe.js
+++ src/commands/Fun/aboutMe.js
@@ -5,11 +5,12 @@ module.exports = {
     name: 'aboutme',
     description: 'Introduces who Sonic the Hedgehog is!',
     callback: async (client, interaction) => {
-        const videoPath = path.join(__dirname, '../images/sonic_jumping.mp4');
+        const videoPath = path.join(__dirname, '..', '..', 'images', 'sonic_jumping.mp4');
         const videoAttachment = new AttachmentBuilder(videoPath);
 
         const embed = new EmbedBuilder()
             .setTitle('About Sonic the Hedgehog')
+            .setThumbnail(client.user.displayAvatarURL())
             .setDescription("I'm Sonic the Hedgehog! The fastest thing alive! I run around saving the world from Dr. Eggman and his robotic army. I love chili dogs and hanging out with my friends Tails and Knuckles!")
             .setColor('#0000FF') // Blue color for Sonic
             .setFooter({ text: 'Gotta go fast!' });
