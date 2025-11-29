3d5ee2d7010ddce6225b52a1054ea756c61f05c2
Anthony <tghuy2000@yahoo.com>
2025-11-28 18:59:02 -0600

    add help command to display a categorized list of bot commands

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -1,51 +1,56 @@
 const { Client, Interaction, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    name: 'help', 
-    description: 'Generate list of commands', 
+    name: 'help',
+    description: 'Generate list of commands',
     callback: async (client, interaction) => {
         const embed = new EmbedBuilder()
-            .setTitle('List of commands')   
+            .setTitle('List of commands')
             .setColor('Random')
             .setThumbnail(client.user.displayAvatarURL())
             .setTimestamp(Date.now())
             .setAuthor({
-                icon: interaction.user.avatar, 
+                icon: interaction.user.avatar,
                 name: interaction.user.tag
             })
             .setFooter({
-                iconURL: client.user.displayAvatarURL(), 
+                iconURL: client.user.displayAvatarURL(),
                 text: client.user.tag
             })
             .addFields([
                 {
-                    name: `Moderation`, 
-                    value: `\`\`kick, ban\`\``,
-                    inline: false
-                }, 
-                {
-                    name: `mathematic`, 
-                    value: `\`\`add, subtract, multiply, divide, sqrt, calculate, log, integrate, sin, cos, tan\`\``, 
-                    inline: false  
-                }, 
-                {
-                    name: `misc`, 
-                    value: `\`\`owner, ping, avatar, weather, translate, userinfo, help, food\`\``, 
+                    name: `Configuration`,
+                    value: `\`\`setupChatbot, disableChatbot\`\``,
                     inline: false
                 },
                 {
-                    name: `Algorithm gameplay`, 
-                    value: `\`\`biggest, smallest, ascending, descending, binarySearch, exponential_search, dfs, roman, remove, duplicate, smallestpositivenumber\`\``, 
+                    name: `Moderation`,
+                    value: `\`\`kick, ban, timeout, unban, untimeout, purge\`\``,
                     inline: false
                 },
                 {
-                    name: `Fun`, 
-                    value: `\`\`meme, say, blackjack, mine\`\``, 
+                    name: `mathematic`,
+                    value: `\`\`add, subtract, multiply, divide, sqrt, calculate, log, integrate, sin, cos, tan\`\``,
                     inline: false
                 },
                 {
-                    name: `Education`, 
-                    value: `\`\`trivia\`\``, 
+                    name: `misc`,
+                    value: `\`\`owner, ping, avatar, weather, translate, userinfo, help, food\`\``,
+                    inline: false
+                },
+                {
+                    name: `Algorithm gameplay`,
+                    value: `\`\`biggest, smallest, ascending, descending, binarySearch, exponential_search, dfs, roman, remove, duplicate, smallestpositivenumber\`\``,
+                    inline: false
+                },
+                {
+                    name: `Fun`,
+                    value: `\`\`meme, say, blackjack, mine\`\``,
+                    inline: false
+                },
+                {
+                    name: `Education`,
+                    value: `\`\`trivia\`\``,
                     inline: false
                 },
                 {
@@ -68,8 +73,8 @@ module.exports = {
                     value: `\`\`riddle, cipher, sudoku, hangman\`\``,
                     inline: false
                 }
-            ])
-    
-        interaction.reply({ embeds: [embed] })
+            ]);
+
+        interaction.reply({ embeds: [embed] });
     }
-}
+};
