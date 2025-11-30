4991125ae6e725ca213b6d5659db9bcf4e4ea8bb
Anthony <tghuy2000@yahoo.com>
2025-11-30 14:27:58 -0600

    feat: add Deck class for managing playing cards with reset, shuffle, and draw functionality

--- src/commands/Fun/Blackjack/Deck.js
+++ src/commands/Fun/Blackjack/Deck.js
@@ -3,7 +3,7 @@ const Card = require('./Card');
 class Deck {
     constructor() {
         this.cards = [];
-        this.suits = ['♥️', '♦️', '♣️', '♠️'];
+        this.suits = ['♥', '♦', '♣', '♠'];
         this.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
         this.reset();
     }
